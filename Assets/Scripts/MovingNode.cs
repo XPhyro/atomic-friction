6dc1c108490d35dca3c3feb5dd5666c5326258a1
Berke Kocaoğlu <berkekocaoglu360@gmail.com>
2019-11-11 08:18:30 +0300

    Add basic functionality
    Added basic graphics management and calculus utilities

---
+++ Assets/Scripts/MovingNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class MovingNode : Node
+{
+    private void Update()
+    {
+        UpdateForce();
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Not yet implemented.");
+    }
+
+    private void UpdateForce()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// Epsilon in eV.
+    /// </summary>
+    public const float Epsilon = 0.84f;
+    /// <summary>
+    /// R0 in m.
+    /// </summary>
+    public const float R0 = 2.56e-10f;
+    /// <summary>
+    /// Spring constant.
+    /// </summary>
+    public const float K = 1f;
+
+    private void Start()
+    {
+        NodeManager.Singleton.Initialise();
+    }
+}
